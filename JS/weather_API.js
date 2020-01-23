c8eed5d28b054f6e66f78b740587764654990f7d
Clinton Davis <you@example.com>
2020-01-23 14:27:32 +0000

    added weather API code and is working

---
+++ JS/weather_API.js
@@ -0,0 +1,24 @@
+const url =
+	"https://api.openweathermap.org/data/2.5/weather?id=1085599&APPID=93fc6b46edd4b57ed1e18af04d5ead0c&units=metric";
+
+const my_id = "&APPID=93fc6b46edd4b57ed1e18af04d5ead0c";
+var unit = "&units=metric";
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	if (this.readyState === 4 && this.status === 200) {
+		var obj = JSON.parse(this.responseText);
+		console.log(obj);
+
+		var city = obj.name;
+		var temps =
+			"H " + obj.main.temp_max + " / " + " L" + " " + obj.main.temp_min;
+		console.log(city);
+		console.log(temps);
+		writeToDoc();
+	}
+	function writeToDoc() {
+		document.getElementById("weather").innerHTML = temps;
+	}
+};
+xhr.open("GET", url + my_id + unit);
+xhr.send();
