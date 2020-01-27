407938e999ff6de661ddf9fc6305b36c16d2d1c8
Clinton-Davis <clintongadavis@gmail.com>
2020-01-27 21:50:27 +0000

    added if statment for wind driection

--- JS/weather_API.js
+++ JS/weather_API.js
@@ -12,15 +12,52 @@ xhr.onreadystatechange = function() {
 		var city = obj.name;
 		var temps =
 			obj.main.temp_max + " / "  + " " + obj.main.temp_min + " ˚C";
-		var wind = obj.wind.speed + " " + obj.wind.deg;	
+		var wind = obj.wind.speed;
+		var direction = obj.wind.deg;
 		console.log(city , temps , wind);
-		;
+		if (direction > 349 || direction <= 12) {
+			var dir = "N";
+		} else if (direction > 12 && direction <= 34) {
+			var dir = "Nne";
+		} else if (direction > 34 && direction <= 56) {
+			var dir = "NE";
+		} else if (direction > 56 && direction <= 78) {
+			var dir = "Ene";
+		} else if (direction > 78 && direction <= 101) {
+			var dir = "E";
+		} else if (direction > 101 && direction <= 123) {
+			var dir = "Ese";
+		} else if (direction > 123 && direction <= 146) {
+			var dir = "Se";
+		} else if (direction > 146 && direction <= 168) {
+			var dir = "Sse";
+		} else if (direction > 168 && direction <= 191) {
+			var dir = "S";
+		} else if (direction > 191 && direction <= 213) {
+			var dir = "Ssw";
+		} else if (direction > 213 && direction <=236) {
+			var dir = "Sw";
+		} else if (direction > 236 && direction <= 258) {
+			var dir = "Wsw";
+		} else if (direction > 258 && direction <= 281) {
+			var dir = "W";
+		} else if (direction > 281 && direction <= 303) {
+			var dir = "Wnw";
+		} else if (direction > 303 && direction <= 326) {
+			var dir = "Nw";
+		} else if (direction > 326 && direction <= 348) {
+			var dir = "Nnw";	
+		} else  {
+			var dir = "N"	
+		};
 		
 		writeToDoc();
 	}
 	function writeToDoc() {
-		document.getElementById("weather").innerHTML = temps + " Wind" + " " + wind;
+		document.getElementById("weather-api").innerHTML = temps + " Wind" + " " + wind + " " + dir; 
 	}
+	
+		
 };
 xhr.open("GET", url + my_id + unit);
 xhr.send();
