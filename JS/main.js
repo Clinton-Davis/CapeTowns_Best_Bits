d894c8d23714e3f0adb7abf2fea6eee4b6459a85
Clinton-Davis <clintongadavis@gmail.com>
2020-01-28 21:14:03 +0000

    added toggle jquary to divs

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,10 @@
 $(function() {
-	$("#sights").click(function() {});
+	$("#sights").click(function() {
+		$("#hh").toggle(1000);
+		
+	});
+	$("#sights").click(function() {
+		$("#h-con").toggle(1000);
+		
+	});
 });
