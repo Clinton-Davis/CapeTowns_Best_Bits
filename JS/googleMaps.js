0da8b8727d23ab9d9648978cb215890f32b94dd8
Clinton Davis <you@example.com>
2020-01-30 19:41:47 +0000

    adding information onto items added pointer on google maps

--- JS/googleMaps.js
+++ JS/googleMaps.js
@@ -1,7 +1,12 @@
-var map;
+// Initialize and add the map
 function initMap() {
-	map = new google.maps.Map(document.getElementById("tm"), {
-		center: { lat: -34.397, lng: 150.644 },
-		zoom: 10
+	// The location
+	var CableCarStation = { lat: -33.9483307, lng: 18.4007576 };
+	// The map, centered
+	var map = new google.maps.Map(document.getElementById("tm"), {
+		zoom: 13,
+		center: CableCarStation
 	});
+	// The marker, positioned
+	var marker = new google.maps.Marker({ position: CableCarStation, map: map });
 }
