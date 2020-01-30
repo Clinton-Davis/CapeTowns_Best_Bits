4c311349b888aac6d8bb761f32e2ef7745617d15
Clinton Davis <you@example.com>
2020-01-30 13:36:18 +0000

    added class switch on innerblock

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,18 @@
 $(function() {
 	$("#sights").click(function() {
-		$("#hh").toggle(1000);
-		
+		$("#selections").toggle(1000);
 	});
 	$("#sights").click(function() {
 		$("#h-con").toggle(2000);
-		
+	});
+
+	$("#tableMountain").click(function() {
+		$("#data1").show();
+	});
+	$("#tableMountain").click(function() {
+		$("#thumbImg").remove();
+	});
+	$("#tableMountain").click(function() {
+		$("#innerBlock").toggleClass("itemContainer", "itemContainer-colum");
 	});
 });
