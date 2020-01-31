1800258f53da7fe33fbfc45d2a2cd70e3b53ade0
Clinton Davis <you@example.com>
2020-01-31 10:22:31 +0000

    Changing the layout to ul li and mogel

--- JS/googleMaps.js
+++ JS/googleMaps.js
@@ -1,23 +1,19 @@
-
-
 //document.getElementById("tableMountain").addEventListener("click", function() {
-	//var location = { lat: -33.9483307, lng: 18.4007576 };
+//var location = { lat: -33.9483307, lng: 18.4007576 };
 //	console.log(location)
 //	initMap ()
- // });
-
-
+// });
 
 // Initialize and add the map
-function initMap() {
-	
-	// The location
-	var location = { lat: -33.9483307, lng: 18.4007576 };
-	// The map, centered
-	var map = new google.maps.Map(document.getElementById("mapDiv"), {
-		zoom: 13,
-		center: location
-	});
-	// The marker, positioned
-	var marker = new google.maps.Marker({ position: location, map: map });
-}
+//function initMap() {
+
+// The location
+var location = { lat: -33.9483307, lng: 18.4007576 };
+// The map, centered
+var map = new google.maps.Map(document.getElementById("mapDiv"), {
+	zoom: 13,
+	center: location
+});
+// The marker, positioned
+var marker = new google.maps.Marker({ position: location, map: map });
+//}
--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,2 @@
-$(function() {
-	$("#sights").click(function() {
-		$("#selections").toggle(1000);
-		$("#h-con").toggle(1000).delay(2000);
-		return;
-	});
-	// Shows - Map Div / information div. Removes thumb img / adds flex-direction class
-	$("#tableMountain").click(function() {
-		$("#data1").show(2000);
-		$("#thumbImg").remove();
-		$("#innerBlock").toggleClass("itemContainer", "itemContainer-colum");
-	});
-});
+
+	
