8b56155f274ecba93a6a81b4dd110ac293333dc1
Clinton Davis <you@example.com>
2020-02-01 15:43:26 +0000

    added code to locSelctors

---
+++ JS/locSeletor.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#tm").click(function() {
+		$("#selectionPicture").toggle(1000);
+		$("ul li").toggle(1000);
+		$("#map").toggle(4000);
+		$(".information").toggle(4000);
+		$(".info-heading").toggle(4000);
+	});
+});
