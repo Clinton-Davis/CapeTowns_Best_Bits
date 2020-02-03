96f461ae05770e480a2e9270e56b197cb9636638
Clinton Davis <you@example.com>
2020-02-03 10:10:20 +0000

    Update locSeletor.js

--- JS/locSeletor.js
+++ JS/locSeletor.js
@@ -3,7 +3,5 @@ $(function() {
 		$("#selectionPicture").toggle(1000);
 		$("ul li").toggle(1000);
 		$("#map").toggle(4000);
-		$(".information").toggle(4000);
-		$(".info-heading").toggle(4000);
 	});
 });
