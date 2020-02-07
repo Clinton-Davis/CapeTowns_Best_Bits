272717779d2f748397a89c1cb158dc6b4497f5fc
Clinton Davis <you@example.com>
2020-02-07 11:19:26 +0000

    make all the sights seeing and adventure info in JSON's

--- JS/googleMaps.js
+++ JS/googleMaps.js
@@ -1,137 +1,142 @@
 /*=-=-=-=-=-=-SIGHT SEEING INFORMAION OBJECTS-=-=-*/
-//Table Mountain Cable Car
-let TM = {
-	Id: "ChIJdeCOYqJnzB0Rm5YhSFRUI2w",
-	info: `The Table Mountain Aerial Cableway Company has been providing visitors with a world-class experience since October 4, 1929. The company operates in a National Park and World Heritage Site.`
+let sightsJSON = {
+	TM: {
+		Id: "ChIJdeCOYqJnzB0Rm5YhSFRUI2w",
+		info: `The Table Mountain Aerial Cableway Company has been providing visitors with a world-class experience since October 4, 1929. The company operates in a National Park and World Heritage Site.`
+	},
+	//Cape Point
+	CP: {
+		Id: "ChIJAf2PqQkjzB0RTEFJZ9rJSSM",
+		info: `Mistakenly cited as the point where the Atlantic and Indian Oceans meet, Cape Point is, nonetheless, a place like no other. (Incidentally, the two oceans’ meeting point is at Cape Agulhas). At Cape Point Nature Reserve, the mountain and ocean meet in a thunderous clash of water against rock, producing a dramatic landscape inhabited by an incredible diversity of life. As the tip of land juts out like on outstretched arm, the ocean swells around it, and multitudes of birds flock to its pristine beaches and rugged cliffs.`
+	},
+	//Boo-Kaap
+	BK: {
+		Id: "ChIJLSz2VWhnzB0Rpm9vazg9yw4",
+		info: `A popular daytime destination, hillside Bo-Kaap is known for its narrow cobbled streets lined with colorful houses. Local Cape Malay culture is represented at sites like the 1790s-built Auwal Mosque and the Bo-Kaap Museum, with exhibits about the achievements of Muslim immigrants. Nearby, simple restaurants serve curries, roti and other Cape Malay dishes. The Noon Gun cannon is fired daily at midday from Signal Hill.`
+	},
+	// Constainta
+	CON: {
+		Id: "ChIJ29Ra-NNpzB0R03JXGGkr6Bo",
+		info: `Constantia is a wine-producing suburb of Cape Town, South Africa. The Constantia Wine Route links the many wineries dotting the Constantia Valley. Housed in a 17th-century wine estate, Groot Constantia Homestead and Wine Museum includes a manor house with 18th- and 19th-century furnishings, plus collections of drinking vessels and carriages. The Constantia Greenbelt network includes the tree-lined Alphen Trail.`
+	},
+	// Kalk Bay
+	KB: {
+		Id: "ChIJu2x0EhZAzB0RCyIwnb_NXFY",
+		info: `Kalk Bay is a fishing village on the coast of False Bay, South Africa and is now a suburb of greater Cape Town. It lies between the ocean and sharply rising mountainous heights that are buttressed by crags of grey Table Mountain Sandstone. A literal translation from the Dutch/Afrikaans name "Kalkbaai" is "Lime Bay".`
+	},
+	// Boulders Beach/Penguins
+	BL: {
+		Id: "ChIJvxVitEk-zB0RVJetXFk0qww",
+		info: `Sheltered public bay with a sandy beach & boardwalk, plus free-roaming colony of African penguins.`
+	},
+	// Chapmens Peak Drive
+	CHP: {
+		Id: "ChIJVagz7BNpzB0Rv9p0tMDvZsg",
+		info: `Chapman's Peak is the name of a mountain on the western side of the Cape Peninsula, between Hout Bay and Noordhoek. The mountain falls sharply for hundreds of metres into the Atlantic Ocean. A spectacular road, known as Chapman's Peak Drive, hugs the near-vertical face of the mountain.`
+	},
+	// Distrct Six Mus
+	DSM: {
+		Id: "ChIJXdpUhHtnzB0RI8TYhHL5028",
+		info: `Museum tracing the 1970s forced clearance of 60,000 residents & demolition of homes under apartheid.`
+	},
+	// Lions Head
+	LH: {
+		Id: "ChIJ7Tf0rQVnzB0REQJgaykzKEg",
+		info: `Boasting panoramic views of the city, this landmark mountain offers hiking trails & historic graves.`
+	},
+
+	// Robin Island
+	RI: {
+		Id: "ChIJy3XfS2WKzB0RYZDeLkVkb30",
+		info: `Historical landmark known for housing political prisoners such as Nelson Mandela, now with a museum.`
+	}
 };
-//Cape Point
-let CP = {
-	Id: "ChIJAf2PqQkjzB0RTEFJZ9rJSSM",
-	info: `Mistakenly cited as the point where the Atlantic and Indian Oceans meet, Cape Point is, nonetheless, a place like no other. (Incidentally, the two oceans’ meeting point is at Cape Agulhas). At Cape Point Nature Reserve, the mountain and ocean meet in a thunderous clash of water against rock, producing a dramatic landscape inhabited by an incredible diversity of life. As the tip of land juts out like on outstretched arm, the ocean swells around it, and multitudes of birds flock to its pristine beaches and rugged cliffs.`
-};
-//Boo-Kaap
-let BK = {
-	Id: "ChIJLSz2VWhnzB0Rpm9vazg9yw4",
-	info: `A popular daytime destination, hillside Bo-Kaap is known for its narrow cobbled streets lined with colorful houses. Local Cape Malay culture is represented at sites like the 1790s-built Auwal Mosque and the Bo-Kaap Museum, with exhibits about the achievements of Muslim immigrants. Nearby, simple restaurants serve curries, roti and other Cape Malay dishes. The Noon Gun cannon is fired daily at midday from Signal Hill.`
-};
-// Constainta
-let CON = {
-	Id: "ChIJ29Ra-NNpzB0R03JXGGkr6Bo",
-	info: `Constantia is a wine-producing suburb of Cape Town, South Africa. The Constantia Wine Route links the many wineries dotting the Constantia Valley. Housed in a 17th-century wine estate, Groot Constantia Homestead and Wine Museum includes a manor house with 18th- and 19th-century furnishings, plus collections of drinking vessels and carriages. The Constantia Greenbelt network includes the tree-lined Alphen Trail.`
-};
-// Kalk Bay
-let KB = {
-	Id: "ChIJu2x0EhZAzB0RCyIwnb_NXFY",
-	info: `Kalk Bay is a fishing village on the coast of False Bay, South Africa and is now a suburb of greater Cape Town. It lies between the ocean and sharply rising mountainous heights that are buttressed by crags of grey Table Mountain Sandstone. A literal translation from the Dutch/Afrikaans name "Kalkbaai" is "Lime Bay".`
-};
-// Boulders Beach/Penguins
-let BL = {
-	Id: "ChIJvxVitEk-zB0RVJetXFk0qww",
-	info: `Sheltered public bay with a sandy beach & boardwalk, plus free-roaming colony of African penguins.`
-};
-// Chapmens Peak Drive
-let CHP = {
-	Id: "ChIJVagz7BNpzB0Rv9p0tMDvZsg",
-	info: `Chapman's Peak is the name of a mountain on the western side of the Cape Peninsula, between Hout Bay and Noordhoek. The mountain falls sharply for hundreds of metres into the Atlantic Ocean. A spectacular road, known as Chapman's Peak Drive, hugs the near-vertical face of the mountain.`
-};
-// Distrct Six Mus
-let DSM = {
-	Id: "ChIJXdpUhHtnzB0RI8TYhHL5028",
-	info: `Museum tracing the 1970s forced clearance of 60,000 residents & demolition of homes under apartheid.`
-};
-// Lions Head
-let LH = {
-	Id: "ChIJ7Tf0rQVnzB0REQJgaykzKEg",
-	info: `Boasting panoramic views of the city, this landmark mountain offers hiking trails & historic graves.`
-};
-// Robin Island
-let RI = {
-	Id: "ChIJy3XfS2WKzB0RYZDeLkVkb30",
-	info: `Historical landmark known for housing political prisoners such as Nelson Mandela, now with a museum.`
-};
+
 //-=-=-=-=-=--=-=-=-ADVENTURE=-=-=--=-=-=--=-=
-//Kayak Cape Town
-let KAY = {
-	Id: "ChIJG31DwhJnzB0RC9C77ufuck0",
-	info: `Kayaking is vastly becoming one of the top Adventure Activities that Cape Town has to offer. With our guided Kayak Tour in Hout Bay and state of the art equipment - let Kayak Adventures take you on a scenic excursion that you won't forget!`
-};
-//Waterfront Charters - Daily Boat Trips & Private Tours https://waterfrontcharters.co.za/
-let BT = {
-	Id: "ChIJAQAAwFlnzB0RJAucQXCvvXk",
-	info: ""
-};
-//Abseil Table Mountain abseilafrica.co.za
-let ABT = {
-	Id: "ChIJ846ZPb5nzB0RCXS_Tom7p9w",
-	info: `EXPLORE CAPE TOWN AND SURROUNDS WITH US Abseil Africa is OPEN 7 days a week between 08:30 – 16:00. Bad weather may affect our operation status. Book your trips with us – we want to take you on an adventure of a lifetime!`
-};
-//Wineland Ballooning Cape Town kapinfo.com
-let BAL = {
-	Id: "ChIJgfmZnllnzB0RPihXS4A96ZA",
-	info: `Wineland Ballooning offers daily scenic Hot Air Balloon flights over the naturally beautiful Berg River Valley,  one of South Africas famous wine regions.
-	On nearly every flight,  while gently floating away,  can we see majestic Table Mountain in the distance and on particularly spectacular days you may even see the Atlantic Ocean with the  “Cape of Good Hope”  100 km away.
-	Each flight is totally unique and no two flights are exactly the same.`
-};
-//Shark Bookings sharkbookings.com
-let SD = {
-	Id: "ChIJY3iy6x0Rzh0RKtT-ViOf004",
-	info: `Shark Bookings has been in operation since 2003 as a central bookings office for the most reputable shark diving operators in South Africa. We have by far the most competitive prices for shark cage diving in Cape Town & Gansbaai, and we offer trips to Mossel Bay and Durban. We work with only the best operators, all of whom are fully licensed and registered. And best of all, bookings are in real time, with instant confirmation, there is no waiting around, just book, pay and go shark diving. `
-};
-//Zipline - Cape Canopy Tour capecanopytour.co.za
-let ZP = {
-	Id: "ChIJfWMCP3THzR0R_8QVpSATqYM",
-	info: `A zipline canopy tour in the company of soaring eagles, surrounded by breath-taking Cape fynbos panoramas and mountainscapes only an hour’s drive from Cape Town by car could well be exactly what you are looking for. Thrilled zipliners who have completed the course have described it as super safe. One commented that the quality of the structure and equipment was reassuringly high and had made her fear of heights irrelevant.`
-};
-//Scootours Cape Town scootours.co.za
-let SC = {
-	Id: "ChIJAxwYC6dnzB0RihwwcBrKwYE",
-	info: `Enjoy an exhilarating downhill descent on the Monster Mountain® Scooters. Scootours® is not an extreme activity but a unique and family friendly way to experience nature and the surroundings of your holiday destination. Scootours make use of non motorised Monster Mountain® Scooters, making this an eco friendly adventure. These Swiss designed scooters are used in the Alps during their summer season on the ski slopes when the snow has melted.`
-};
-//Lions Head - Hike Addicts hikeaddicts.com
-let HH = {
-	Id: "ChIJ1_u4UbNnzB0RH1tMJWyQWrI",
-	info: `Cape Town is an adventure hotspot featuring an endless variety of exciting activities. At Hike Addicts we are pleased to offer a diverse selection of Cape Town’s most exhilarating tours on one platform.
+let adventureJSON = {
+	//Kayak Cape Town
+	KAY: {
+		Id: "ChIJG31DwhJnzB0RC9C77ufuck0",
+		info: `Kayaking is vastly becoming one of the top Adventure Activities that Cape Town has to offer. With our guided Kayak Tour in Hout Bay and state of the art equipment - let Kayak Adventures take you on a scenic excursion that you won't forget!`
+	},
+	//Waterfront Charters - Daily Boat Trips & Private Tours https://waterfrontcharters.co.za/
+	BT: {
+		Id: "ChIJAQAAwFlnzB0RJAucQXCvvXk",
+		info: `""`
+	},
+	//Abseil Table Mountain abseilafrica.co.za
+	ABT: {
+		Id: "ChIJ846ZPb5nzB0RCXS_Tom7p9w",
+		info: `EXPLORE CAPE TOWN AND SURROUNDS WITH US Abseil Africa is OPEN 7 days a week between 08:30 – 16:00. Bad weather may affect our operation status. Book your trips with us – we want to take you on an adventure of a lifetime!`
+	},
+	//Wineland Ballooning Cape Town kapinfo.com
+	BAL: {
+		Id: "ChIJgfmZnllnzB0RPihXS4A96ZA",
+		info: `Wineland Ballooning offers daily scenic Hot Air Balloon flights over the naturally beautiful Berg River Valley, one of South Africas famous wine regions.
+			On nearly every flight, while gently floating away, can we see majestic Table Mountain in the distance and on particularly spectacular days you may even see the Atlantic Ocean with the  'Cape of Good Hope'  100 km away.
+			Each flight is totally unique and no two flights are exactly the same.`
+	},
+	//Shark Bookings sharkbookings.com
+	SD: {
+		Id: "ChIJY3iy6x0Rzh0RKtT-ViOf004",
+		info: `Shark Bookings has been in operation since 2003 as a central bookings office for the most reputable shark diving operators in South Africa. We have by far the most competitive prices for shark cage diving in Cape Town & Gansbaai, and we offer trips to Mossel Bay and Durban. We work with only the best operators, all of whom are fully licensed and registered. And best of all, bookings are in real time, with instant confirmation, there is no waiting around, just book, pay and go shark diving. `
+	},
+	//Zipline - Cape Canopy Tour capecanopytour.co.za
+	ZP: {
+		Id: "ChIJfWMCP3THzR0R_8QVpSATqYM",
+		info: `A zipline canopy tour in the company of soaring eagles, surrounded by breath-taking Cape fynbos panoramas and mountainscapes only an hour’s drive from Cape Town by car could well be exactly what you are looking for. Thrilled zipliners who have completed the course have described it as super safe. One commented that the quality of the structure and equipment was reassuringly high and had made her fear of heights irrelevant.`
+	},
+	//Scootours Cape Town scootours.co.za
+	SC: {
+		Id: "ChIJAxwYC6dnzB0RihwwcBrKwYE",
+		info: `Enjoy an exhilarating downhill descent on the Monster Mountain® Scooters. Scootours® is not an extreme activity but a unique and family friendly way to experience nature and the surroundings of your holiday destination. Scootours make use of non motorised Monster Mountain® Scooters, making this an eco friendly adventure. These Swiss designed scooters are used in the Alps during their summer season on the ski slopes when the snow has melted.`
+	},
+	//Lions Head - Hike Addicts hikeaddicts.com
+	HH: {
+		Id: "ChIJ1_u4UbNnzB0RH1tMJWyQWrI",
+		info: `Cape Town is an adventure hotspot featuring an endless variety of exciting activities. At Hike Addicts we are pleased to offer a diverse selection of Cape Town’s most exhilarating tours on one platform.
 			Table Mountain shows off its exceptional beauty all year round and for the more adventurous souls, hiking Table Mountain is possibly the most rewarding and amazing experience you will have in Cape Town.`
-};
-//Cape Toewn Helicotpers helicopterscapetown.co.za
-let HT = {
-	Id: "ChIJN4QZT1ZnzB0Rq1OQmhhKoFc",
-	info: ""
+	},
+	//Cape Toewn Helicotpers helicopterscapetown.co.za
+	HT: {
+		Id: "ChIJN4QZT1ZnzB0Rq1OQmhhKoFc",
+		info: `""`
+	}
 };
 // locationFinder is the onClick function to initMap on clicked Location
 function locationFinder(locationFinder_id) {
 	console.log(locationFinder_id);
 	switch (locationFinder_id) {
-		case "tm":
-			(iD = TM.Id), (info = TM.info), initMap();
+		case "tm": // Sights starts
+			(iD = sightsJSON.TM.Id), (info = sightsJSON.TM.info), initMap();
 			break;
 		case "cp":
-			(iD = CP.Id), (info = CP.info), initMap();
+			(iD = sightsJSON.CP.Id), (info = sightsJSON.CP.info), initMap();
 			break;
 		case "bk":
-			(iD = BK.Id), (info = BK.info), initMap();
+			(iD = sightsJSON.BK.Id), (info = sightsJSON.BK.info), initMap();
 			break;
 		case "con":
-			(iD = CON.Id), (info = CON.info), initMap();
+			(iD = sightsJSON.CON.Id), (info = sightsJSON.CON.info), initMap();
 			break;
 		case "kb":
-			(iD = KB.Id), (info = KB.info), initMap();
+			(iD = sightsJSON.KB.Id), (info = sightsJSON.KB.info), initMap();
 			break;
 		case "bl":
-			(iD = BL.Id), (info = BL.info), initMap();
+			(iD = sightsJSON.BL.Id), (info = sightsJSON.BL.info), initMap();
 			break;
 		case "chp":
-			(iD = CHP.Id), (info = CHP.info), initMap();
+			(iD = sightsJSON.CHP.Id), (info = sightsJSON.CHP.info), initMap();
 			break;
 
 		case "dsm":
-			(iD = DSM.Id), (info = DSM.info), initMap();
+			(iD = sightsJSON.DSM.Id), (info = sightsJSON.DSM.info), initMap();
 			break;
 		case "lh":
-			(iD = LH.Id), (info = LH.info), initMap();
+			(iD = sightsJSON.LH.Id), (info = sightsJSON.LH.info), initMap();
 			break;
 		case "ri":
-			(iD = RI.Id), (info = RI.info), initMap();
+			(iD = sightsJSON.RI.Id), (info = sightsJSON.RI.info), initMap();
 			break; //addventure starts
 		case "bt":
 			(iD = BT.Id), (info = BT.info), initMap();
@@ -156,7 +161,7 @@ function locationFinder(locationFinder_id) {
 			break;
 		case "ht":
 			(iD = HT.Id), (info = HT.info), initMap();
-			break;
+			break; // Foodies Strars
 	}
 
 	function initMap() {
