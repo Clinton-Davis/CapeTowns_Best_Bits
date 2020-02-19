c66f5baafd6a30910f0e24cab685057ca16138b5
Clinton Davis <you@example.com>
2020-02-19 18:02:01 +0000

    added fadeIn and Out with times

--- JS/mainPageFunctions.js
+++ JS/mainPageFunctions.js
@@ -1,38 +1,61 @@
 $(function() {
 	// Removing main heading and adding colums in
 	$("#sights").click(function() {
-		$("h1,h2,h3,p").addClass("hide");
-		$("#sightsCol").removeClass("hide");
-		$("#adventureCol").addClass("hide");
-		$("#foodieCol").addClass("hide");
-		$("#map").addClass("hide");
+		$("h1,h2,h3,p").fadeOut(200, function() {
+			$("h1,h2,h3,p").addClass("hide");
+			$("#sights").addClass("hide");
+			$("#adventure").addClass("hide");
+			$("#foodie").addClass("hide");
+			$("#sightsCol").fadeIn(200, function() {
+				$("#sightsCol").removeClass("hide");
+			});
+		});
 	});
-	$("#adventure").click(function() {
-		$("#adventureCol").removeClass("hide");
-		$("h1,h2,h3,p").addClass("hide");
+	/*	$("#adventure").click(function() {
+		$("#mainHeading").fadeOut(200, function() {
+			/*$("#adventureCol").fadeIn(200, function() {
+				$("#mainHeading")
+					.children()
+					.addClass("hide");
+				$(
+		});
 		$("#sightsCol").addClass("hide");
 		$("#foodieCol").addClass("hide");
 		$("#map").addClass("hide");
 	});
 	$("#foodie").click(function() {
-		$("h1,h2,h3,p").addClass("hide");
-		$("#sightsCol").addClass("hide");
-		$("#adventureCol").addClass("hide");
 		$("#foodieCol").removeClass("hide");
-		$("#map").addClass("hide");
-	});
-	// Back Botton fuctions for colums
-	$("#sback").click(function() {
+		$("#mainHeading")
+			.children()
+			.addClass("hide");
 		$("#sightsCol").addClass("hide");
-		$("h1,h2,h3,p").removeClass("hide");
-	});
-	$("#aback").click(function() {
 		$("#adventureCol").addClass("hide");
-		$("h1,h2,h3,p").removeClass("hide");
+		$("#map").addClass("hide");
+	}); */
+	// Flag Colse button fuctions for colums
+	$("#sback").click(function() {
+		$("#sightsCol").fadeOut(200, function() {
+			$("#sightsCol").addClass("hide");
+			$("h1,h2,h3,p").fadeIn(200, function() {
+				$("h1,h2,h3,p").removeClass("hide");
+				$("#sights").removeClass("hide");
+				$("#adventure").removeClass("hide");
+				$("#foodie").removeClass("hide");
+			});
+		});
+	});
+	/*
+	$("#aback").click(function() {
+		$("#mainHeading")
+			.children()
+			.removeClass("hide");
+		$("#adventureCol").addClass("hide");
 	});
 	$("#fback").click(function() {
 		$("#foodieCol").addClass("hide");
-		$("h1,h2,h3,p").removeClass("hide");
+		$("#mainHeading")
+			.children()
+			.removeClass("hide");
 	});
 
 	// Click on selection to show maps
@@ -56,7 +79,9 @@ $(function() {
 	});
 	//Back botton in maps
 	$("#back").click(function() {
-		$("h1,h2,h3,p").removeClass("hide");
+		$("#mainHeading")
+			.children()
+			.removeClass("hide");
 		$("#map").addClass("hide");
 		$("#back").addClass("hide");
 		$("#directions").addClass("hide");
@@ -64,5 +89,5 @@ $(function() {
 	$("#about").click(function() {
 		$("#map").addClass("hide");
 		$("#back").addClass("hide");
-	});
+	});*/
 });
