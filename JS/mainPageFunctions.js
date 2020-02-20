67592c3ee6fb36dff1a2094fdcc3b5a68e516819
Clinton Davis <you@example.com>
2020-02-20 10:32:23 +0000

    added delay with jq on mdal about

--- JS/mainPageFunctions.js
+++ JS/mainPageFunctions.js
@@ -48,91 +48,81 @@ $(function() {
 		});
 	});
 
-		// Removing main heading and adding Foodie col
-		$("#foodie").click(function() {
-			$("h1,h2,h3,p").fadeOut(200, function() {
-				$("h1,h2,h3,p").addClass("hide");
-				$("#sights").addClass("hide");           
-				$("#adventure").addClass("hide");
-				$("#foodie").addClass("hide");
-				$("#foodieCol").fadeIn(200, function() {
-					$("#foodieCol").removeClass("hide");
-				});
+	// Removing main heading and adding Foodie col
+	$("#foodie").click(function() {
+		$("h1,h2,h3,p").fadeOut(200, function() {
+			$("h1,h2,h3,p").addClass("hide");
+			$("#sights").addClass("hide");
+			$("#adventure").addClass("hide");
+			$("#foodie").addClass("hide");
+			$("#foodieCol").fadeIn(200, function() {
+				$("#foodieCol").removeClass("hide");
 			});
 		});
-		// Flag Close button for Foodie col
-		$("#fback").click(function() {
-			$("#foodieCol").fadeOut(200, function() {
-				$("#foodieCol").addClass("hide");
-				$("h1,h2,h3,p").fadeIn(200, function() {
-					$("h1,h2,h3,p").removeClass("hide");
-					$("#sights").removeClass("hide");
-					$("#adventure").removeClass("hide");
-					$("#foodie").removeClass("hide");
-				});
-			});
-		});
-
-	/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-GOOGLE MAPS SECTION-=-=-=-=-=-=-=-*/
-
-	// Click on selection to show maps
-	$("li").click(function() {
-		$("#sightsCol").fadeOut(200, function(){
-			$("#sightsCol").addClass("hide");
-		})
-		$("#back").fadeIn(200, function(){
-			$("#back").removeClass("hide");
-			$("#map").removeClass("hide");
-		})
-		
 	});
-	$("li").click(function() {
-		$("#adventureCol").fadeOut(200, function(){
-			$("#adventureCol").addClass("hide");
-		})
-		$("#back").fadeIn(200, function(){
-			$("#back").removeClass("hide");
-			$("#map").removeClass("hide");
-		})
-		
-	});
-	$("li").click(function() {
-		$("#foodieCol").fadeOut(200, function(){
+	// Flag Close button for Foodie col
+	$("#fback").click(function() {
+		$("#foodieCol").fadeOut(200, function() {
 			$("#foodieCol").addClass("hide");
-		})
-		$("#back").fadeIn(200, function(){
-			$("#back").removeClass("hide");
-			$("#map").removeClass("hide");
-		})
-		
-	});
-	
-	
-	
-	
-	//Back botton in maps
-	$("#back").click(function() {
 			$("h1,h2,h3,p").fadeIn(200, function() {
 				$("h1,h2,h3,p").removeClass("hide");
 				$("#sights").removeClass("hide");
 				$("#adventure").removeClass("hide");
 				$("#foodie").removeClass("hide");
-				$("#map").addClass("hide");
-				$("#back").css("display", "none");
 			});
-		
+		});
 	});
-	
-	//About Botton
+
+	/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-GOOGLE MAPS SECTION-=-=-=-=-=-=-=-*/
+
+	// Click on selection to show maps
+	$("li").click(function() {
+		$("#sightsCol").fadeOut(200, function() {
+			$("#sightsCol").addClass("hide");
+		});
+		$("#back").fadeIn(200, function() {
+			$("#back").removeClass("hide");
+			$("#map").removeClass("hide");
+		});
+	});
+	$("li").click(function() {
+		$("#adventureCol").fadeOut(200, function() {
+			$("#adventureCol").addClass("hide");
+		});
+		$("#back").fadeIn(200, function() {
+			$("#back").removeClass("hide");
+			$("#map").removeClass("hide");
+		});
+	});
+	$("li").click(function() {
+		$("#foodieCol").fadeOut(200, function() {
+			$("#foodieCol").addClass("hide");
+		});
+		$("#back").fadeIn(200, function() {
+			$("#back").removeClass("hide");
+			$("#map").removeClass("hide");
+		});
+	});
+
+	//Back botton in maps
+	$("#back").click(function() {
+		$("h1,h2,h3,p").fadeIn(200, function() {
+			$("h1,h2,h3,p").removeClass("hide");
+			$("#sights").removeClass("hide");
+			$("#adventure").removeClass("hide");
+			$("#foodie").removeClass("hide");
+			$("#map").addClass("hide");
+			$("#back").css("display", "none");
+		});
+	});
+
+	//Modal About Botton Open
 	$("#about").click(function() {
-		$("#map").addClass("hide");
-		$("#back").addClass("hide");
+		$("#aboutModal").fadeIn(1000);
+		$("#mapCon").css("display", "none");
+		$("#back").css("display", "none");
 	});
-	
+	$("#modal-close").click(function() {
+		$("#aboutModal").fadeOut(1000);
+	});
 });
-
-
-
-	
-
-
--- JS/modal.js
+++ JS/modal.js
@@ -1,19 +1,21 @@
-// Get the modal
+/*Get the modal 
 var modal = document.getElementById("aboutModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("about");
+var mapCon = document.getElementById("mapCon");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// <span> To  closes modal
+var closeBtn = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
 	modal.style.display = "block";
+	mapCon.style.display = "none";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeBtn.onclick = function() {
 	modal.style.display = "none";
 };
 
@@ -23,3 +25,4 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 };
+*/
