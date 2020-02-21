c27e8394384c3c77343b96d45449b450ac295fa0
Clinton Davis <you@example.com>
2020-02-21 15:54:09 +0000

    added side nav to open with javascrits and media on tems

---
+++ JS/nav.js
@@ -0,0 +1,22 @@
+const navslide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav");
+	const navLinks = document.querySelectorAll(".nav li");
+	//toggle nav
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
+		//animate links
+		navLinks.forEach((links, index) => {
+			if (links.style.animation) {
+				links.style.animation = ``;
+			} else {
+				links.style.animation = `navLinkFade 0.5s ease forwards ${index / 8 +
+					0.5}s`;
+			}
+		});
+		//Burger Animation
+		burger.classList.toggle("toggle");
+	});
+};
+
+navslide();
