1553b46ac3d88cd1dda8ba9ffa4fba25b9f666f4
Clinton Davis <you@example.com>
2020-02-27 12:17:06 +0000

    added more deg's on xx

--- JS/nav.js
+++ JS/nav.js
@@ -3,20 +3,22 @@ const navslide = () => {
 	const burger = document.querySelector(".burger");
 	const nav = document.querySelector(".nav");
 	const navLinks = document.querySelectorAll(".nav li");
-	//toggle nav
+
 	burger.addEventListener("click", () => {
+		//toggle nav
 		nav.classList.toggle("nav-active");
-		//Burger Animation
-		burger.classList.toggle("toggle");
+
 		//animate links
 		navLinks.forEach((links, index) => {
 			if (links.style.animation) {
-				links.style.animation = ``;
+				links.style.animation = "";
 			} else {
 				links.style.animation = `navLinkFade 0.5s ease forwards ${index / 8 +
 					0.5}s`;
 			}
 		});
+		//burger Ainmatin
+		burger.classList.toggle("toggle");
 	});
 };
 
