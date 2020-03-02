903cac2e73d9203b3819d6b3dcd2f3a40709e400
Clinton Davis <you@example.com>
2020-03-02 13:26:23 +0000

    added spinner to apper when clicked and disapear when finished loading

--- JS/listener.js
+++ JS/listener.js
@@ -1,30 +1,32 @@
+function addSpinner() {
+	$("#googleIcon").css("display", "block");
+}
+
 /*
 1. making a new MO object
 2. tell it what to observe
 3. what to do with the observed changes/mutatutions
 */
-const myList = document.querySelector('#directionsPanel');
+const myList = document.querySelector("#directionsPanel");
 /*1*/
 const observer = new MutationObserver(mutations => {
-    mutations.forEach(record => {
-        console.log(record);
+	mutations.forEach(record => {
+		console.log(record);
 
-        if (record.type === 'attributes') {
-            changepic();
-        }else {console.log('MutatuinObserver has Failed');
-    }
-    })
-    
-    
-/*try to get it to do somethnog*/
+		if (record.type === "attributes") {
+			changepic();
+		} else {
+			console.log("MutatuinObserver has Failed");
+		}
+	});
+
+	/*try to get it to do somethnog*/
 });
 /*function that removes the loadeer image when Google Api loads directions*/
 function changepic() {
-    $("#loader").css("display", "none");
-    
+	$("#googleIcon").css("display", "none");
 }
 /*2*/
 observer.observe(myList, {
-    attributes:true
+	attributes: true
 });
-
