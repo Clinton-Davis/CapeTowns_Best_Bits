6a7918a8c8056dc40bea08fcfc083fa5f1987b7e
Clinton Davis <you@example.com>
2020-03-04 14:34:53 +0000

    added comments to code to make it easer to understand

--- JS/listener.js
+++ JS/listener.js
@@ -1,15 +1,11 @@
+/*Adds Spinner and loading divs to page when GetDirections is clicked*/
 function addSpinner() {
 	$("#googleIcon").css("display", "block");
 	$("#loadingText").css("display", "block");
 }
-
-/*
-1. making a new MO object
-2. tell it what to observe
-3. what to do with the observed changes/mutatutions
-*/
+/* This is a MutationObdrtver to see then the directions have been loaded by
+	the google-api */
 const myList = document.querySelector("#directionsPanel");
-/*1*/
 const observer = new MutationObserver(mutations => {
 	mutations.forEach(record => {
 		console.log(record);
@@ -20,10 +16,9 @@ const observer = new MutationObserver(mutations => {
 			console.log("MutatuinObserver has Failed");
 		}
 	});
-
-	/*try to get it to do somethnog*/
 });
-/*function that removes the loadeer image when Google Api loads directions*/
+/*This function removes the loader Div and spinner image when Google Api 
+  has finished*/
 function changepic() {
 	$("#googleIcon").css("display", "none");
 	$("#loadingText").css("display", "none");
